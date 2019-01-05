b8111656ccef74699adfce53d861bb35d8b1fee8
Atilla YAVUZ <atillayavuz@gmail.com>
2019-01-05 14:37:11 +0300

    Adds fetch data from api

--- AspNetCoreApp/AspNetCoreApp.Api/react-app/src/App.js
+++ AspNetCoreApp/AspNetCoreApp.Api/react-app/src/App.js
@@ -3,23 +3,32 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    baseUrl: "http://localhost:2684/",
+    tasks: []
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    const url = this.state.baseUrl + "api/Task";
+    fetch(url)
+      .then(response => response.json())
+      .then(tasks => this.setState({ tasks }));
+  }
+
   render() {
+    const { tasks } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {tasks.map(task => {
+              return <span key={task.id}>{task.title}</span>;
+            })}
           </p>
-
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
