f4a44f23379aee55e82227df6dd85ce12d71ee50
Atilla YAVUZ <atillayavuz@gmail.com>
2019-01-05 17:01:18 +0300

    Adds post task to api

--- AspNetCoreApp/AspNetCoreApp.Api/react-app/src/App.js
+++ AspNetCoreApp/AspNetCoreApp.Api/react-app/src/App.js
@@ -3,30 +3,95 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  state = {
-    baseUrl: "http://localhost:2684/",
-    tasks: []
-  };
-
+  constructor(props) {
+    super(props);
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
+    this.handleChangeDescription = this.handleChangeDescription.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      title: "",
+      description: "",
+      baseUrl: "http://localhost:2684/",
+      tasks: []
+    };
+  }
   componentDidMount() {
-    this.fetchData();
+    this.getTasks();
   }
 
-  fetchData() {
+  getTasks() {
     const url = this.state.baseUrl + "api/Task";
     fetch(url)
       .then(response => response.json())
       .then(tasks => this.setState({ tasks }));
   }
 
+  handleChangeTitle(e) {
+    this.setState({ title: e.target.value });
+  }
+
+  handleChangeDescription(e) {
+    this.setState({ description: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const url = this.state.baseUrl + "api/Task";
+    const { title, description } = this.state;
+
+    fetch("http://localhost:2684/api/Task", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ Title: title, Description: description })
+    })
+      .then(task => {
+        debugger;
+        if (task.status == 200) {
+          this.setState({ title: "", description: "" });
+          this.getTasks();
+        }
+      })
+      .catch(err => console.log);
+  }
+
   render() {
-    const { tasks } = this.state;
+    const { tasks, title, description } = this.state;
     return (
       <div className="App">
         <header className="App-header">
+          <form onSubmit={this.handleSubmit}>
+            <div className={"form-group" + (!title ? " has-error" : "")}>
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                className="form-control"
+                name="title"
+                value={title}
+                onChange={this.handleChangeTitle}
+              />
+              {/* {submitted && !taskModel.title && (
+                <div className="help-block">Username is required</div>
+              )} */}
+              <label htmlFor="description">Description</label>
+              <input
+                type="text"
+                className="form-control"
+                name="description"
+                value={description}
+                onChange={this.handleChangeDescription}
+              />
+              <div className="form-group">
+                <button className="btn btn-primary">Save</button>
+              </div>
+            </div>
+          </form>
           <p>
             {tasks.map(task => {
-              return <span key={task.id}>{task.title}</span>;
+              return (
+                <span key={task.id}>{task.title + " " + task.description}</span>
+              );
             })}
           </p>
         </header>
